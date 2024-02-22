a8b55c6b961df25b868b33ba6affe8b6b7c97314
mgwinnier <mgwinnier@gmail.com>
2024-02-22 16:48:20 -0600

    routing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter and alias it as Router
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router> {/* Use Router here, which is actually HashRouter */}
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
