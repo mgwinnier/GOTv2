c7b55dd8e2d61e20ae3e0d31f9e9f0651269cff7
mgwinnier <mgwinnier@gmail.com>
2024-02-22 17:40:59 -0600

    Tweaks

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import heroBackground from '../assets/hero-background.jpg';
 import '../styles/herosection.css';
 
 function HeroSection() {
-  const fullTextSubtitle = "Ensuring silver linings of golden years";
-  const [subtitleText, setSubtitleText] = useState("");
+    const fullTextSubtitle = "Ennsuring silver linings of golden years";
+    const [subtitleText, setSubtitleText] = useState("");
+    const indexRef = useRef(0); // Use useRef to keep track of the current index
 
-  useEffect(() => {
-    let index = 0;
-    const timeoutId = setInterval(() => {
-      setSubtitleText((prev) => prev + fullTextSubtitle[index]);
-      index++;
-      if (index === fullTextSubtitle.length) clearInterval(timeoutId);
-    }, 75); // Adjust the speed of typing here
+    useEffect(() => {
+        setSubtitleText(""); // Clear subtitle text on component mount
+        indexRef.current = 0; // Reset index to 0 on component mount
+        const timeoutId = setInterval(() => {
+            if (indexRef.current < fullTextSubtitle.length) {
+                setSubtitleText(prev => prev + fullTextSubtitle.charAt(indexRef.current));
+                indexRef.current += 1; // Increment the index
+            } else {
+                clearInterval(timeoutId); // Clear interval when the end of the string is reached
+            }
+        }, 75); // Adjust the speed of typing here
 
-    return () => clearInterval(timeoutId); // Cleanup on component unmount
-  }, [fullTextSubtitle]);
+        return () => clearInterval(timeoutId); // Cleanup on component unmount
+    }, []); // Empty dependency array ensures this effect runs only once when the component mounts
 
-  return (
-    <div className="relative w-full bg-cover bg-center bg-no-repeat text-white" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${heroBackground})`, height: '60vh' }}>
-      <div className="container mx-auto flex flex-col items-center justify-center h-full">
-        <div className="text-center px-4 md:px-0">
-          {/* Main Title (static) */}
-          <h1 className=" text-2xl md:text-3xl xl:text-6xl font-game-of-thrones leading-tight text-yellow-400 shadow-md">Gramps of Tennessee</h1>
-          {/* Subtitle with Typewriter Effect */}
-          <div className="typewriter">
-            <p className="mt-2 text-xl md:text-xl xl:text-3xl font-light text-gray-300 shadow-lg">{subtitleText}</p>
-          </div>
-          {/* Learn More Button */}
-          <Link to="/about" className="mt-8 md:mt-10 inline-block bg-yellow-500 text-black font-medium py-3 px-6 text-lg rounded-lg transition duration-300 hover:bg-yellow-600 hover:text-white shadow-md">Learn More</Link>
+    return (
+        <div className="relative w-full bg-cover bg-center bg-no-repeat text-white" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${heroBackground})`, height: '60vh' }}>
+            <div className="container mx-auto flex flex-col items-center justify-center h-full">
+                <div className="text-center px-4 md:px-0">
+                    <h1 className="text-2xl md:text-3xl xl:text-6xl font-game-of-thrones leading-tight text-yellow-400 shadow-md">Gramps of Tennessee</h1>
+                    <div className="typewriter">
+                        <p className="mt-2 text-xl md:text-xl xl:text-3xl font-light text-gray-300 shadow-lg">{subtitleText}</p>
+                    </div>
+                    <Link to="/about" className="mt-8 md:mt-10 inline-block bg-yellow-500 text-black font-medium py-3 px-6 text-lg rounded-lg transition duration-300 hover:bg-yellow-600 hover:text-white shadow-md">Learn More</Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default HeroSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -15,7 +15,6 @@ import RealEstateImg from '../assets/real-estate.jpg';
 import CounselingImg from '../assets/Counseling.jpg';
 import DrivingImg from '../assets/driving.jpg';
 import FoodImg from '../assets/meal-preparation.jpg';
-import TransImg from '../assets/transportation.jpg';
 // Add other imports for your images...
 
 const advisingServices = [
@@ -36,23 +35,21 @@ const otherServices = [
   { name: "Counseling", img: CounselingImg },
   { name: "Driving Services", img: DrivingImg },
   { name: "Meal Preparation", img: FoodImg },
-  { name: "Transportation", img: TransImg },
   // Add other services here...
 ];
 
 function ServicesSection() {
-  const [showAdvising, setShowAdvising] = useState(true); // Default to true
-  const [showOtherServices, setShowOtherServices] = useState(true); // Default to true
+  const [showAdvising, setShowAdvising] = useState(true);
+  const [showOtherServices, setShowOtherServices] = useState(true);
 
   return (
     <section id="services" className="py-12 bg-white">
       <div className="container mx-auto px-4 text-center">
         
-        {/* Advising Services Section */}
-        <div className="mb-8">
-          <h2 className="text-3xl font-bold text-gray-800">Advising Services</h2>
+        <div>
+          <h2 className="text-3xl font-bold text-gray-800 mb-4">Advising Services</h2>
           <button 
-            className="mt-4 bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 transition duration-300"
+            className="mt-4 bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 transition duration-300 mb-8"
             onClick={() => setShowAdvising(!showAdvising)}
           >
             {showAdvising ? 'Show Less' : 'Learn More'}
@@ -69,36 +66,25 @@ function ServicesSection() {
           </div>
         )}
 
-        {/* Other Services Section */}
         <div>
-          <h3 className="text-3xl font-bold text-gray-800 mb-8">Other Services</h3>
+          <h3 className="text-3xl font-bold text-gray-800 mb-4">Other Services</h3>
           <button 
-            className="mt-4 bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 transition duration-300"
+            className="mt-4 bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600 transition duration-300 mb-8"
             onClick={() => setShowOtherServices(!showOtherServices)}
           >
             {showOtherServices ? 'Show Less' : 'Learn More'}
           </button>
-          {showOtherServices && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-              {otherServices.map((service, index) => (
-                <div key={index} className="flex flex-col items-center">
-                  <img src={service.img} alt={service.name} className="w-64 h-64 object-cover rounded-full shadow-lg mb-2 transition-transform duration-300 hover:scale-105"/>
-                  <p className="font-bold text-lg">{service.name}</p>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
-
-        {/* 'Click for more information' Button */}
-        <div className="mt-8">
-          <Link to="/services-page">
-            <button className="bg-gold-500 text-white font-bold py-3 px-6 rounded text-lg hover:bg-gold-600 transition duration-300" style={{ fontSize: '1.125rem', backgroundColor: '#D4AF37' }}>
-              Click to see our resources
-            </button>
-          </Link>
-        </div>
-
+        {showOtherServices && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            {otherServices.map((service, index) => (
+              <div key={index} className="flex flex-col items-center">
+                <img src={service.img} alt={service.name} className="w-64 h-64 object-cover rounded-full shadow-lg mb-2 transition-transform duration-300 hover:scale-105"/>
+                <p className="font-bold text-lg">{service.name}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
