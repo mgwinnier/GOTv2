aedcd353cb224aa6558adbe8911276a8a4a53640
mgwinnier <mgwinnier@gmail.com>
2024-08-23 07:55:11 -0500

    Update whoweare.jsx

--- src/whoweare.jsx
+++ src/whoweare.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AnnaImage from './assets/anna.jpg'; // Ensure the path is correct
+import susieImage from './assets/resources/susie.jpg'
 
 const WhoWeAre = () => {
   return (
@@ -9,13 +9,13 @@ const WhoWeAre = () => {
       <div className="flex justify-center items-start">
         <div className="w-full max-w-md flex flex-col items-center bg-white border-double border-4 border-yellow-500 rounded-lg shadow-lg overflow-hidden transform transition duration-500 hover:scale-105">
           <div className="p-4 w-full flex justify-center">
-            <img src={AnnaImage} alt="Anna Laboy" className="object-cover rounded-md" style={{ height: '250px', maxWidth: '100%' }} />
+            <img src={susieImage} alt="Susie Lerma" className="object-cover rounded-md" style={{ height: '250px', maxWidth: '100%' }} />
           </div>
           <div className="bg-white p-6 flex flex-col justify-between space-y-4">
             <h2 className="text-2xl font-semibold text-yellow-500">Anna Laboy</h2>
             <p className="text-sm text-gray-600">Owner & Senior Specialist</p>
             <p className="text-gray-700 text-base">
-              Anna Laboy is a Murfreesboro native with a passion for seniors & her community! Her GRAMPS team has over 4 decades of service in the senior sector & consists of specialists in various areas pertaining to our golden years.
+            Susie Lerma is a Murfreesboro native with a passion for seniors & her community! Her GRAMPS team has over 4 decades of service in the senior sector & consists of specialists in various areas pertaining to our golden years.
               Our goal is to walk beside and relieve the family of any stressors that may arise with changing needs and empower them with knowledge.
               We strive to provide assistance to seniors & their families by taking them step by step through the entire journey of the aging process.
             </p>
