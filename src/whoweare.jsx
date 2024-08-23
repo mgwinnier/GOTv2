dabe319bcaa37277523b27e41f4cb538f6ca100c
mgwinnier <mgwinnier@gmail.com>
2024-08-23 08:02:46 -0500

    susie

--- src/whoweare.jsx
+++ src/whoweare.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import susieImage from './assets/resources/susie.jpg'
+import susieImage from './assets/susie.jpg'
 
 const WhoWeAre = () => {
   return (
@@ -12,10 +12,10 @@ const WhoWeAre = () => {
             <img src={susieImage} alt="Susie Lerma" className="object-cover rounded-md" style={{ height: '250px', maxWidth: '100%' }} />
           </div>
           <div className="bg-white p-6 flex flex-col justify-between space-y-4">
-            <h2 className="text-2xl font-semibold text-yellow-500">Anna Laboy</h2>
+            <h2 className="text-2xl font-semibold text-yellow-500">Susie Eskew</h2>
             <p className="text-sm text-gray-600">Owner & Senior Specialist</p>
             <p className="text-gray-700 text-base">
-            Susie Lerma is a Murfreesboro native with a passion for seniors & her community! Her GRAMPS team has over 4 decades of service in the senior sector & consists of specialists in various areas pertaining to our golden years.
+            Susie Eskew is a Murfreesboro native with a passion for seniors & her community! Her GRAMPS team has over 4 decades of service in the senior sector & consists of specialists in various areas pertaining to our golden years.
               Our goal is to walk beside and relieve the family of any stressors that may arise with changing needs and empower them with knowledge.
               We strive to provide assistance to seniors & their families by taking them step by step through the entire journey of the aging process.
             </p>
